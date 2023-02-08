414d63f3b4b7280373137e87905bd611ee7ffc74
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-08 14:17:33 -0800

    auth styling

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -3,17 +3,17 @@ import { useState } from 'react';
 import { useUserContext } from '../../context/UserContext.js';
 import { useParams, NavLink, Redirect } from 'react-router-dom';
 import { authUser } from '../../services/auth';
+import './Auth.css';
 
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+// import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
 
 export default function Auth() {
   const { type } = useParams();
@@ -23,7 +23,7 @@ export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const theme = createTheme();
+  // const theme = createTheme();
 
   if (user) {
     return <Redirect to="/" />;
@@ -31,17 +31,15 @@ export default function Auth() {
 
   function Copyright(props) {
     return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      <Typography variant="body2" align="center" {...props}>
         {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          To-DoLittle
-        </Link>{' '}
-        {new Date().getFullYear()}
+        Crooked Looks {new Date().getFullYear()}
         {'.'}
       </Typography>
     );
   }
-  const submitAuthHandler = async () => {
+  const submitAuthHandler = async (e) => {
+    e.preventDefault();
     try {
       const newUser = await authUser(email, password, type);
       setUser(newUser);
@@ -50,61 +48,56 @@ export default function Auth() {
     }
   };
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <nav>
-            <NavLink to="/auth/sign-up" variant="body2" className="nav-link">
-              Sign Up
-            </NavLink>
-            <NavLink to="/auth/sign-in">Sign In</NavLink>
-            <Box component="form" onSubmit={submitAuthHandler} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
+    <>
+      <div className="align">
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} className="starting-component">
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography>Sign in</Typography>
+        <nav className="auth-nav-container">
+          <NavLink to="/auth/sign-up" className="nav-link">
+            Sign Up
+          </NavLink>
+          <NavLink to="/auth/sign-in" className="nav-link">
+            Sign In
+          </NavLink>
+        </nav>
+        <div>
+          <form className="form auth" onSubmit={submitAuthHandler}>
+            <div className="form__field">
+              <input
                 required
-                fullWidth
-                id="email"
+                type="text"
+                className="form__input"
+                placeholder="Email"
+                // id="email"
                 label="Email Address"
                 name="email"
                 autoComplete="email"
                 autoFocus
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                margin="normal"
+              ></input>
+              <input
                 required
-                fullWidth
                 name="password"
                 label="Password"
-                type="password"
-                id="password"
+                type="text"
+                placeholder="Password"
+                // id="password"
                 autoComplete="current-password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button variant="contained" onClick={submitAuthHandler}>
-                Submit
-              </Button>
-            </Box>
-          </nav>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+                className="form__input"
+              ></input>
+            </div>
+            <Button variant="contained" onClick={submitAuthHandler}>
+              Submit
+            </Button>
+          </form>
+        </div>
+      </div>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </>
   );
 }
