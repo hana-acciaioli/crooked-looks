bf8f51cdcf2f1106a5dd746d28c388da40056d1f
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-07 17:33:10 -0800

    showList @media styling and date display edited

--- src/components/Shows/ShowList.js
+++ src/components/Shows/ShowList.js
@@ -8,21 +8,65 @@ import './ShowList.css';
 export default function VideoList() {
   const { user } = useUserContext();
   const { shows } = useUIContext();
+
+  function dateToString(date) {
+    const [year, month, day] = date.split('-');
+    const dateObj = { month, day, year };
+    let long = '';
+    {
+      dateObj.month === '01' && (long = 'Jan');
+    }
+    {
+      dateObj.month === '02' && (long = 'Feb');
+    }
+    {
+      dateObj.month === '03' && (long = 'Mar');
+    }
+    {
+      dateObj.month === '04' && (long = 'April');
+    }
+    {
+      dateObj.month === '05' && (long = 'May');
+    }
+    {
+      dateObj.month === '06' && (long = 'June');
+    }
+    {
+      dateObj.month === '07' && (long = 'Jul');
+    }
+    {
+      dateObj.month === '08' && (long = 'Aug');
+    }
+    {
+      dateObj.month === '09' && (long = 'Sept');
+    }
+    {
+      dateObj.month === '10' && (long = 'Oct');
+    }
+    {
+      dateObj.month === '11' && (long = 'Nov');
+    }
+    {
+      dateObj.month === '12' && (long = 'Dec');
+    }
+
+    return long + ' ' + dateObj.day;
+  }
   return (
     <div>
-      {/* {user &&  */}
-      <ShowForm />
-      {/* } */}
+      {user && <ShowForm />}
       <div className="show-showcase">
-        <h2>Upcoming Shows</h2>
+        {/* <h2>Upcoming Shows</h2> */}
         {shows.map((show) => (
           <div key={show.id} className="show-container">
-            <div className="date-container">{show.date}</div>
-            <div className="location-container">{show.location}</div>
-            <div className="city-container">{show.city}</div>
-            <Button as="a" href={show.link} className="link-button">
-              Details
-            </Button>
+            <div className="date-container">{dateToString(show.date)}</div>
+            <div className="details-container">
+              <div className="location-container">{show.location}</div>
+              <div className="city-container">{show.city}</div>
+              <Button as="a" href={show.link} className="link-container">
+                Details
+              </Button>
+            </div>
           </div>
         ))}
       </div>
