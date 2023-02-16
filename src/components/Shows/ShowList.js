d9573c0104f96349ac1661b8dd078890d2086a96
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-15 18:17:49 -0800

    show list only showing upcoming shows

--- src/components/Shows/ShowList.js
+++ src/components/Shows/ShowList.js
@@ -8,7 +8,7 @@ import { deleteShow } from '../../services/shows.js';
 
 export default function VideoList() {
   const { user } = useUserContext();
-  const { shows, setShows, upcomingShows, setUpcomingShows } = useUIContext();
+  const { setShows, upcomingShows } = useUIContext();
 
   function dateToString(date) {
     const [year, month, day] = date.split('-');
@@ -64,24 +64,6 @@ export default function VideoList() {
   return (
     <div>
       {user && <ShowForm />}
-      <div className="show-showcase">
-        {/* <h2>Upcoming Shows</h2> */}
-        {shows.map((show) => (
-          <>
-            <div key={show.id} className="show-container">
-              <div className="date-container">{dateToString(show.date)}</div>
-              <div className="details-container">
-                <div className="location-container">{show.location}</div>
-                <div className="city-container">{show.city}</div>
-                <Button as="a" href={show.link} className="link-container">
-                  Details
-                </Button>
-              </div>
-            </div>
-            {user && <Button onClick={() => handleDelete(show)}>Delete</Button>}
-          </>
-        ))}
-      </div>
       <div className="show-showcase">
         {/* <h2>Upcoming Shows</h2> */}
         {upcomingShows.map((show) => (
