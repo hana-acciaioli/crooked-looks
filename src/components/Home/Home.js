c85b7af02a6aa1e8165422a4991446df3f340b1e
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-15 16:00:19 -0800

    replaced video background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './Home.css';
 export default function Home() {
   return (
-    <div>
+    <div className="showcase">
       {' '}
-      <section className="showcase">
-        <video muted loop autoPlay playsInline>
-          <source src="/assets/videos/CrookedLooksBurn1080RESIZED.mp4" type="video/mp4" />
-        </video>
-        <div className="overlay"></div>
+      {/* <section className="showcase"> */}
+      <video muted loop autoPlay playsInline className="showcase-video">
+        <source src="/assets/videos/zoomed-crooked-looks.mp4" type="video/mp4" />
+      </video>
+      {/* <div className="overlay"></div>
         <div className="text">
           {/* <h2>Crooked Looks </h2> */}
-          {/* <h4>Rock n Roll band straight outta Portland, OR</h4> */}
-          <p></p>
-          {/* <a href="#">Listen</a> */}
-        </div>
-      </section>
+      {/* <h4>Rock n Roll band straight outta Portland, OR</h4> */}
+      <p></p>
+      {/* <a href="#">Listen</a> */}
+      {/* </div> */}
+      {/* </section> */}
     </div>
   );
 }
