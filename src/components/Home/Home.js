965a74176e9d6049def56a1dfa4083a4f4c73368
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-15 16:16:29 -0800

    media menu size adjustment

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,18 +4,9 @@ export default function Home() {
   return (
     <div className="showcase">
       {' '}
-      {/* <section className="showcase"> */}
       <video muted loop autoPlay playsInline className="showcase-video">
         <source src="/assets/videos/zoomed-crooked-looks.mp4" type="video/mp4" />
       </video>
-      {/* <div className="overlay"></div>
-        <div className="text">
-          {/* <h2>Crooked Looks </h2> */}
-      {/* <h4>Rock n Roll band straight outta Portland, OR</h4> */}
-      <p></p>
-      {/* <a href="#">Listen</a> */}
-      {/* </div> */}
-      {/* </section> */}
     </div>
   );
 }
