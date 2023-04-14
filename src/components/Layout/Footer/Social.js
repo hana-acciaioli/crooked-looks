d0c5f3501bc729a097920ba69190b323a30a3bff
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-04-13 21:29:05 -0700

    socials footer in place with gradiant background

--- src/components/Layout/Footer/Social.js
+++ src/components/Layout/Footer/Social.js
@@ -1,31 +1,97 @@
 import React from 'react';
 import './Social.css';
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaTiktok,
+  FaSpotify,
+  FaYoutube,
+  FaBandcamp,
+} from 'react-icons/fa';
 
 export default function Social() {
   return (
-    <div className="social-container">
-      <ul className="social">
-        <li>
-          <a href="https://m.facebook.com/CrookedLooksBand/">
-            <img src="https://i.ibb.co/x7P24fL/facebook.png" />
+    <footer className="social-footer">
+      <ul className="socials-list">
+        <li className="highlight">
+          <a
+            data-toggle="tooltip"
+            title=""
+            name="Facebook"
+            data-placement="top"
+            href="https://m.facebook.com/CrookedLooksBand/"
+          >
+            <FaFacebook className="styletest" />
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/TheCrookedLooks">
-            <img src="https://i.ibb.co/Wnxq2Nq/twitter.png" />
+          <a
+            data-toggle="tooltip"
+            title=""
+            name="Twitter"
+            data-placement="top"
+            href="https://twitter.com/TheCrookedLooks"
+          >
+            <FaTwitter />
           </a>
         </li>
         <li>
-          <a href="https://www.instagram.com/thecrookedlooks/?hl=en">
-            <img src="https://i.ibb.co/ySwtH4B/instagram.png" />
+          <a
+            data-toggle="tooltip"
+            title=""
+            name="Instagram"
+            data-placement="top"
+            href="https://www.instagram.com/thecrookedlooks/?hl=en"
+          >
+            <FaInstagram />
           </a>
         </li>
         <li>
-          <a href="https://www.tiktok.com/@thecrookedlooks?lang=en">
-            <img src="/tik-tok.png" className="social-img" />
+          <a
+            data-toggle="tooltip"
+            title=""
+            name="Tiktok"
+            data-placement="top"
+            href="https://www.tiktok.com/@thecrookedlooks?lang=en"
+          >
+            <FaTiktok />
+          </a>
+        </li>
+        <li>
+          <a
+            title=""
+            name="Spotify"
+            data-placement="top"
+            href="https://open.spotify.com/artist/0WdHCAdm2XxKMfEz8Zfa6k?go=1&sp_cid=f01ad1ec0a2aaad84cf0f5d2b841d2d2&utm_source=embed_player_p&utm_medium=desktop&nd=1"
+          >
+            <FaSpotify />
+          </a>
+        </li>
+        <li>
+          <a
+            title=""
+            name="Youtube"
+            data-placement="top"
+            href="https://www.youtube.com/channel/UCmD6DthoalU4OqPLHKHwxaQ"
+          >
+            <FaYoutube />
+          </a>
+        </li>
+        <li>
+          <a
+            title=""
+            name="Bandcamp"
+            data-placement="top"
+            href="https://crookedlooks.bandcamp.com/"
+          >
+            <FaBandcamp />
           </a>
         </li>
       </ul>
-    </div>
+      {/* <div className="text-center copyright">
+        <p>© 2023 Crooked Looks </p>
+      </div> */}
+    </footer>
   );
 }
